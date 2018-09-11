d3851290d48e99a3ef5daca889c3256b279227ee
kgavhane702 <kgavhane702@gmail.com>
2018-09-11 13:32:43 +0530

    Singletone

--- JavaCodingAssignment 4/src/com/core/MySingleTonClass.java
+++ JavaCodingAssignment 4/src/com/core/MySingleTonClass.java
@@ -8,6 +8,21 @@ public class MySingleTonClass {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public static MySingleTonClass ge
+	public static MySingleTonClass getInstance()
+	{
+		  if(myObj == null){
+	            myObj = new MySingleTonClass();
+	        }
+	        return myObj;
+	}
 	
+	  public void getSomeThing(){
+	        // do something here
+	        System.out.println("I am here....");
+	    }
+	
+	  public static void main(String a[]){
+	        MySingleTonClass st = MySingleTonClass.getInstance();
+	        st.getSomeThing();
+	    }
 }
